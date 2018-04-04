89929e860a23ced2304bee8c5df28c11ee8d2846
Hannah Lee and Jasun Feddema <jasun@>
2018-04-04 11:00:27 -0700

    workon navbar.

---
+++ src/app/project.model.ts
@@ -0,0 +1,11 @@
+export class Project {
+  constructor (
+    public name: string,
+    public starters: string,
+    public description: string,
+    public goal: number,
+    public plan: string,
+    public userRewards: string,
+    public category: string
+  ) { }
+}
